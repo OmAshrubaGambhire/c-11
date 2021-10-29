e98679e91cfe54534139d0489c7273a3d58c2117
OmAshrubaGambhire <92320591+OmAshrubaGambhire@users.noreply.github.com>
2021-10-29 20:56:12 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var weight = [35,38,42,45,43,34,36,41,48,32];
+
+
+
+function setup() {
+  createCanvas(400,400);
+
+for(i=0;1<weight.length;i++){
+  sum = sum + weight[i];
+}
+
+}
+
+function draw() 
+{
+  background(30);
+}
+
+ 
+
